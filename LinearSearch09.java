2771df0a2961078395f2fc5ba0e499d25e03f890
evanparisya <144124511+evanparisya@users.noreply.github.com>
2023-10-25 09:56:41 +0700

    modifikasi daspro jobsheet 9

--- LinearSearch09.java
+++ LinearSearch09.java
@@ -1,21 +1,37 @@
+import java.util.Scanner;
 /**
  * LinearSearch09
  */
 public class LinearSearch09 {
 
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan jumlah elemen array : ");
+        int Jml = input.nextInt();
+        int[] arrayInt = new int[Jml];
+        int key;
         int hasil = 0;
 
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
-                hasil = i;
-                break;
-            }
+            System.out.print("Masukkan elemen array ke-"+(i+1)+" : ");
+            arrayInt[i] = input.nextInt();
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        System.out.print("Masukkan key yang ingin dicari : ");
+        key = input.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+             if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+             }
+        }
+
+        if (hasil == 0) {
+            System.out.println("Key tidak ditemukan dalam array");
+        } else {
+             System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        }
+       
     }
 }
