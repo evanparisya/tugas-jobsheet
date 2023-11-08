7117e8152e6795f43c7bdf8d8763530ef709bfff
evanparisya <144124511+evanparisya@users.noreply.github.com>
2023-11-08 10:39:19 +0700

    jobsheet11

---
+++ Tugas2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * Tugas2
+ */
+public class Tugas2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        for(int i = 1; i<=N; i++){
+            for(int j = N; j>=i; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
